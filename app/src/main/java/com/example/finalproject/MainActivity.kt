26fd134fa386d0307c7196d72e3973c7382c6454
Dipesh Poudel <dipesh.poudel33@gmail.com>
2021-03-10 13:36:18 +0545

    Register and login

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -1,11 +1,103 @@
 package com.example.finalproject
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+
 
 class MainActivity : AppCompatActivity() {
+    private val permissions = arrayOf(
+            android.Manifest.permission.CAMERA,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+    )
+    private lateinit var etemail: EditText
+    private  lateinit var etpassword: EditText
+    private lateinit var btnlogin: Button
+    private lateinit var btnsignup: Button
+    private lateinit var constrantlayout: ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        etemail = findViewById(R.id.etEmail)
+        btnlogin = findViewById(R.id.btnLogin)
+        etpassword = findViewById(R.id.etPassword)
+        btnsignup = findViewById(R.id.btnSignup)
+        constrantlayout = findViewById(R.id.constrantlayout)
+        checkRunTimePermission()
+
+        btnlogin.setOnClickListener {
+            login()
+
+        }
+
+        btnsignup.setOnClickListener {
+            startActivity(Intent(this@MainActivity, SignupActivity::class.java))
+        }
+    }
+
+    private fun checkRunTimePermission() {
+        if (!hasPermission()) {
+            requestPermission()
+        }
+    }
+
+    private fun hasPermission(): Boolean {
+        var hasPermission = true
+        for (permission in permissions) {
+            if (ActivityCompat.checkSelfPermission(
+                            this,
+                            permission
+                    ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                hasPermission = false
+                break
+            }
+        }
+        return hasPermission
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this@MainActivity, permissions, 1)
+    }
+
+    private fun login() {
+        val email = etemail.text.toString()
+        val password = etpassword.text.toString()
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val repository = UserRepository()
+                val response = repository.loginUser(email, password)
+                if (response.success == true) {
+                    ServiceBuilder.token = "Bearer " + response.token
+                    startActivity(
+                            Intent(
+                                    this@MainActivity,
+                                    DashboardActivity::class.java
+                            )
+                    )
+                    finish()
+                } else {
+                    withContext(Dispatchers.Main) {
+                        val snack =
+                                Snackbar.make(
+                                        constrantlayout,
+                                        "Invalid credentials",
+                                        Snackbar.LENGTH_LONG
+                                )
+                        snack.setAction("OK", View.OnClickListener {
+
+                        })
+                        snack.show()
+                    }
+                }
+
+            } catch (ex: Exception) {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(
+                            this@MainActivity,
+                            "Login error", Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
     }
 }
